2f73a0512127aa3fec17921315a02de701e5385f
Marco De Moja <marco.moja@dr.local>
2017-11-01 16:56:59 +0000

    wip

--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -2,26 +2,33 @@ import React from 'react'
 import { Container, Header, Tab } from 'semantic-ui-react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-import { switchTab } from './actions'
+import { switchTab, showMiniPreview } from './actions'
+import { ListContainer } from 'ListContainer'
+import { itemProps } from '../prop-types/itemProps'
 
 
 class App extends Component {
   tabPanes =[
     {
       menuItem: 'Puppies',
-      render: () => <Tab.Pane loading>put my component here</Tab.Pane>
+      render: () => <Tab.Pane loading><ListContainer onShowMiniPreview={this.pro} items={this.props.puppies} /></Tab.Pane>
     },
     {
       menuItem: 'Kitties',
-      render: () => <Tab.Pane loading>put my component here</Tab.Pane>
+      render: () => <Tab.Pane loading><ListContainer items={this.props.kitties} /></Tab.Pane>
     }
   ]
 
   handleSwitchTab = (e, {activeTab}) => this.props.dispatchOnSwitchTab(activeTab)
 
+  handleShowMiniPreview = (e, {previewObj}) => this.props.dispatchOnShowMiniPreview(previewObj)
+
   static propTypes = {
-    dispatchOnSwitchTab: React.PropTypes.func,
-    activeTab: PropTypes.number
+    dispatchOnSwitchTab: PropTypes.func,
+    dispatchOnShowMiniPreview: PropTypes.func,
+    activeTab: PropTypes.number,
+    puppies: PropTypes.arrayOf(itemProps),
+    kitties: PropTypes.arrayOf(itemProps)
   }
 
   render() {
@@ -43,6 +50,9 @@ function mapStateToProps(state) {
     return {
       dispatchOnSwitchTab: (tabIndex) => {
         dispatch(switchTab(tabIndex))
+      },
+      dispatchOnShowMiniPreview: (item) => {
+        dispatch(showMiniPreview(item))
       }
 
     }
--- src/containers/ListContainer.js
+++ src/containers/ListContainer.js
@@ -2,34 +2,32 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import itemProps from '../prop-types/itemProps'
 import { List } from 'semantic-ui-react'
-import {ItemContainer} from 'ItemContainer'
 
 class ListContainer extends React.Component {
 
-    static propTypes = {
-        items: PropTypes.arrayOf(itemProps)
+  static propTypes = {
+    items: PropTypes.arrayOf(itemProps),
+    miniPreview: PropTypes.object,
+    onShowMiniPreview: PropTypes.func,
+  }
+
+  fetchItems() {
+    let itemsList = []
+    let items = this.props.items
+    for (let i in items) {
+      itemsList.push(<List.Item>
+          <List.Content>
+            <List.Header as='h4'>Header</List.Header>
+            <List.Description><Image src='/assets/images/wireframe/short-paragraph.png' /></List.Description>
+          </List.Content>
+        </List.Item>)
     }
+    return items
+  }
 
-    fetchItems() {
-      let itemsList = []
-      let items = this.props.items
-      for(let i in items) {
-        itemsList.push(<ItemContainer key={i} ...item[i] />)
-      }
-    }
-
-    render(){
-        return (<List>
-        </List>)
-    }
-}
-
-function mapStateToProps() {
-
-}
-
-function mapDispatchToProps() {
-
+  render() {
+    return (<List>{this.fetchItems()}</List>)
+  }
 }
 
 export default ListContainer
