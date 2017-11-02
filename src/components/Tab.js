686401b0756f6a3d42a62ec7d7bc96e9e928e8d4
Marco De Moja <marco.moja@dr.local>
2017-11-02 16:58:30 +0000

    wip

--- src/components/Tab.js
+++ src/components/Tab.js
@@ -4,12 +4,16 @@ import PropTypes from 'prop-types'
 class Tab extends React.Component {
 
   static propTypes = {
-    panes: PropTypes.arrayOf(PropTypes.node)
+    panes: PropTypes.arrayOf(PropTypes.node),
+    activePane: PropTypes.bool
   }
 
   constructor(props) {
     super(props)
+    this.switchTab = this._switchTab.bind(this)
   }
+
+  _switchTab() {}
 
   renderButtons() {
     return this.props.panes.map((pane, i) => <button key={i} className='tablinks' onClick={false}></button>)
